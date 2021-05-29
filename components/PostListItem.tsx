0e76272ad58698561e7365abf292b67b19d1ad7c
Brycen Dorsay <dorsay@live.ca>
2021-05-29 13:48:26 -0700

    made full post preview clickable

--- components/PostListItem.tsx
+++ components/PostListItem.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from 'react';
 import { format } from 'date-fns';
 import Link from 'next/link';
+import styles from '../styles/PostListItem.module.scss';
 
 type Props = {
   title: string;
@@ -13,15 +14,13 @@ const PostListItem: FunctionComponent<Props> = ({ title, date, preview, slug }) 
   const formattedDate = format(new Date(date), 'dd MMM yyyy');
 
   return (
-    <div className="py-24">
-      <Link href={`/post/${slug}`}>
-        <h3 className="text-3xl font-bold hover:text-red-400 mb-2 cursor-pointer transition-colors">
-          {title}
-        </h3>
-      </Link>
-      <p className="text-gray-400 mb-5">{formattedDate}</p>
-      <p>{preview}</p>
-    </div>
+    <Link href={`/post/${slug}`}>
+      <div className={`${styles.postListItem} py-24 cursor-pointer`}>
+        <h3 className="text-3xl font-bold mb-2 transition-colors">{title}</h3>
+        <p className="text-gray-400 mb-5">{formattedDate}</p>
+        <p>{preview}</p>
+      </div>
+    </Link>
   );
 };
 
