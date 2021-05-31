8f42315ab796f794ade444af6e51589a50625446
Brycen Dorsay <dorsay@live.ca>
2021-05-30 21:48:50 -0700

    potential fix for permissions

--- webhook/fetchPosts.ts
+++ webhook/fetchPosts.ts
@@ -1,5 +1,6 @@
 import { AxiosInstance } from 'axios';
 import { GhostPost, GhostPostsResponse, Post } from './types';
+import { setTimeout as sleep } from 'timers/promises';
 
 function normalize(ghostPost: GhostPost) {
   const post: Post = {
@@ -15,8 +16,22 @@ function normalize(ghostPost: GhostPost) {
   return post;
 }
 
+async function fetch(client: AxiosInstance) {
+  const maxAttempts = 10;
+  for (let i = 0; i < maxAttempts; i++) {
+    try {
+      const result = await client.get<GhostPostsResponse>('/posts?formats=html');
+      return result;
+    } catch (err) {
+      console.error(err.response.statusText);
+      if (i < maxAttempts) await sleep(i * 100);
+    }
+  }
+  throw new Error('Error fetching posts');
+}
+
 export default async function fetchPosts(client: AxiosInstance) {
-  const posts = await client.get<GhostPostsResponse>('/posts?formats=html');
+  const posts = await fetch(client);
   const normalizedPosts = posts.data.posts.map(normalize);
 
   return normalizedPosts;
--- webhook/index.ts
+++ webhook/index.ts
@@ -3,10 +3,10 @@ import fetchPosts from './fetchPosts';
 import fs from 'fs';
 import createToken from './createToken';
 import axios from 'axios';
-import { setTimeout as sleep } from 'timers/promises';
+import { Post } from './types';
 
-const apiKey = process.env.API_KEY;
 const baseURL = `http://ghost:2368/ghost/api/v3/admin`;
+const apiKey = process.env.API_KEY;
 
 const token = createToken(apiKey);
 console.log(token);
@@ -18,35 +18,28 @@ const client = axios.create({
   },
 });
 
-async function writePosts() {
-  const maxAttempts = 100;
-  for (let i = 0; i < maxAttempts; i++) {
-    try {
-      const posts = await fetchPosts(client);
-      const stringified = JSON.stringify(posts, null, 2);
-      fs.writeFileSync('posts.json', stringified);
-      break;
-    } catch (err) {
-      console.log(err);
-      if (i >= maxAttempts - 1) console.error('Error getting posts');
-      else await sleep(i * 100);
-    }
-  }
+async function writePosts(posts: Post[]) {
+  const stringified = JSON.stringify(posts, null, 2);
+  fs.writeFileSync('posts.json', stringified);
 }
 
 const handler: RequestListener = async function handler(_, res) {
   console.log('Rebuilding site');
 
-  await writePosts();
+  const posts = await fetchPosts(client);
+  await writePosts(posts);
 
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify({ message: 'Success' }));
 };
 
 async function main() {
-  await writePosts();
+  const posts = await fetchPosts(client);
+  await writePosts(posts);
+
   const server = createServer(handler);
   server.listen(80);
+
   console.log('Listening for webhooks on 80');
 }
 main();
