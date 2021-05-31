5499dfd8758eebcfe60f96c5db341ba3b3c49c6f
Brycen Dorsay <dorsay@live.ca>
2021-05-30 21:27:46 -0700

    nullable post preview

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,7 @@ export interface Post {
   title: string;
   slug: string;
   content: string;
-  preview: string;
+  preview: string | null;
   created_at: string;
   updated_at: string;
   published_at: string | null;
