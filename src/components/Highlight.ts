b27cd7da620692147ed7b57f5bba8d918cb2cb23
Brycen Dorsay <dorsay@live.ca>
2022-05-16 21:27:23 -0700

    bump modules

--- src/components/Highlight.ts
+++ src/components/Highlight.ts
@@ -1,23 +1,9 @@
-import { highlightAll, registerLanguage } from 'highlight.js';
-import shell from 'highlight.js/lib/languages/shell';
-import bash from 'highlight.js/lib/languages/bash';
-import javascript from 'highlight.js/lib/languages/javascript';
-import typescript from 'highlight.js/lib/languages/typescript';
-import html from 'highlight.js/lib/languages/xml';
-import yaml from 'highlight.js/lib/languages/yaml';
-import json from 'highlight.js/lib/languages/json';
-import 'highlight.js/styles/tomorrow-night-eighties.css';
+import hljs from 'highlight.js/lib/common';
+import type { HighlightedHTMLElement, HighlightResult } from 'highlight.js';
+import 'highlight.js/styles/base16/tomorrow-night.css';
 
-registerLanguage('shell', shell);
-registerLanguage('bash', bash);
-registerLanguage('javascript', javascript);
-registerLanguage('typescript', typescript);
-registerLanguage('html', html);
-registerLanguage('yaml', yaml);
-registerLanguage('json', json);
-
-export default function highlightt() {
-  highlightAll();
+export default function highlight() {
+  hljs.highlightAll();
   const blocks = document.querySelectorAll<HighlightedHTMLElement>('pre code.hljs');
   blocks.forEach(block => {
     if (block.result === undefined) return;
